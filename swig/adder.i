66ad25f4a07c6708780fc7e66fb237b06b28ccc1
Mark Kohler <mark.kohler@gmail.com>
2013-02-17 15:03:52 -0800

    Swig _sr functions and write tests.

--- swig/adder.i
+++ swig/adder.i
@@ -3,4 +3,18 @@
 #include "adder.h"
 %}
 
-%include "adder.h"
+#define ADDER_VERSION "v1.0"
+
+int add(int, int);
+char * get_version(void);
+char * make_greeting(char *name);
+
+%include "typemaps.i"
+/* Calling the parameter OUTPUT makes it so. */
+int add_sr(int x, int y, int *OUTPUT);
+
+%include "cstring.i"
+%cstring_output_maxsize(char * out_str,  int buflen);
+int get_version_sr(char * out_str, int buflen);
+
+int make_greeting_sr(char * name, char * out_str, int buflen);
--- swig/test_adder.py
+++ swig/test_adder.py
@@ -1,14 +1,36 @@
 #!/usr/bin/python
 import os
 import sys
-print "working directory", os.getcwd()
-print "sys.path:", sys.path
 import adder
 from nose.tools import eq_
 
 
-def test_adder():
+def test_add():
     eq_(adder.add(2, 3), 5)
+
+def test_add_sr():
+    eq_(adder.add_sr(2, 3), [0, 5])
+
+
+def test_get_version():
+    eq_(adder.get_version(), adder.ADDER_VERSION)
+
+def test_get_version_sr_small_buf():
+    eq_(adder.get_version_sr(2), [1, ''])
+
+def test_get_version_sr():
+    eq_(adder.get_version_sr(64), [0, adder.ADDER_VERSION])
+
+
+def test_make_greeting():
+    eq_(adder.make_greeting("Python"), "Hello, Python")
+
+def test_make_greeting_sr_small_buf():
+    eq_(adder.make_greeting_sr("Python", 5), [1, ""])
+
+def test_make_greeting_sr():
+    eq_(adder.make_greeting_sr("Python", 64), [0, "Hello, Python"])
+
 
 if __name__ == '__main__':
     import nose
